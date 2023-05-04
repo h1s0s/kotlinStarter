007973dff4448cc25706980b17108fbb916a5663
한상선 <hs94122@naver.com>
2023-05-04 19:40:00 +0900

    Initial commit

---
+++ src/main/kotlin/example/Person.kt
@@ -0,0 +1,15 @@
+package example
+
+/**
+ * packageName  : example
+ * fileName     : Person
+ * author       : sshan
+ * date         : 2023-05-04
+ * description  :
+ * ========================================================
+ *  DATE            AUTHOR              NOTE
+ * --------------------------------------------------------
+ * 2023-05-04          sshan            최초생성
+ */
+class Person {
+}
