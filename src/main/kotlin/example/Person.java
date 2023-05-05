aa9b0eb9be06fcbf4d88bfeeac4c1303e1880413
한상선 <hs94122@naver.com>
2023-05-05 21:27:26 +0900

    type을 길들이기

--- src/main/kotlin/example/Person.java
+++ src/main/kotlin/example/Person.java
@@ -4,9 +4,14 @@ import org.jetbrains.annotations.Nullable;
 
 public class Person {
      private final String name;
+     private int age;
 
      public Person(String name){
          this.name = name;
+     }
+     public Person(String name, int age){
+         this.name = name;
+         this.age = age;
      }
 
      @Nullable
